9da70c646d9b5846fd2889745ce33a36742b00a2
Tu T. Do <tu.dothanh1906@gmail.com>
2023-07-05 21:32:51 +0700

    Adding PTH TO DATASET ARG

--- train.sh
+++ train.sh
@@ -1,6 +1,11 @@
 PORT=30001
 GPU=$1
 CFG=$2
-TAG=${3:-'default'}
+PTH=$3
+TAG=${4:-'default'}
 
-torchrun --nproc_per_node $GPU --master_port $PORT main.py --cfg $CFG --data-path <path-to-imagenet> --amp --tag $TAG
+torchrun --nproc_per_node $GPU \
+	--master_port $PORT main.py \
+	--cfg $CFG \
+	--data-path $PTH \
+	--amp --tag $TAG
